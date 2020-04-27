2c4a6df0e9f36966199a6bd181324a4c5a7f440d
MoniqueCampbell <43890502+MoniqueCampbell@users.noreply.github.com>
2020-04-27 13:52:49 -0500

    lab4

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
+
 import os
 from app import app
 from flask import render_template, request, redirect, url_for, flash, session, abort
